66f189eb29be01b0ae71e742a2cb7994ece298cb
Christian <christian@twobulls.com>
2016-08-23 07:42:29 +1000

    Add platform sources

--- Include/Cplus/Type/Object/Class.h
+++ Include/Cplus/Type/Object/Class.h
@@ -5,10 +5,17 @@
 #include "Cplus/Base.h"
 #include "Cplus/Type/TypeNumber.h"
 
+typedef struct k_FileLocation_t
+{
+	k_String fileName;
+	int lineNumber;
+	int column;
+} k_FileLocation;
+
 typedef struct k_Class_t
 {
 	k_Base base;
-	k_Registry registry;
+	k_Registry *registry;
 	k_String name;
 	k_TypeNumber typeNumber;
 	k_Vector properties;
@@ -17,4 +24,13 @@ typedef struct k_Class_t
 	k_Pool *pool;
 } k_Class;
 
-extern k_Allocator k_Class_Alloc;
+k_Class *k_Class_New(k_Registry *);
+void k_Class_Destroy(k_Class *);
+
+void k_Class_Instance_Construct(k_Class *, k_Any, ...);
+k_Any k_Class_Instance_New(k_Class *, ...);
+k_Any k_Class_Instance_New2(k_Class *, k_Allocator *, ...);
+void k_Class_Instance_Destroy(k_Class *, k_Any);
+
+
+
--- Source/Debug.c
+++
@@ -1,2 +0,0 @@
-#include "Cplus/Debug/Assert.h"
-
